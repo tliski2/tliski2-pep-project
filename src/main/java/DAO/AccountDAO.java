de5b85ee62590c4eacae572124bbdb415f02a44d
Tyler Liskiewicz <tyler.liskiewicz@gmail.com>
2025-04-14 07:31:11 +0000

    insertAccount implementation

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -5,11 +5,33 @@ import Util.ConnectionUtil;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Account DAO class to handle the user login/registration data from the front-end and interact with the database
  */
 
 public class AccountDAO {
+
+    /*
+     * Attempts to insert new user account into the database
+     * 
+     * @param account to be inserted
+     * @return the newly inserted account or null if unsuccessful
+     * @throws SQLException
+     * 
+     */
+    public Account insertAccount(Account account) throws SQLException {
+        Connection connection = ConnectionUtil.getConnection();
+        String sql = "INSERT INTO account (username, password) VALUES (?, ?)";
+        try(PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, account.getUsername());
+            ps.setString(2, account.getPassword());
+
+            int rows = ps.executeUpdate();
+            return rows > 0 ? account : null;
+        }
+
+    }
     
 }
