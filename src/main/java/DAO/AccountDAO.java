48e13eac93f24dabf15834acce40243bbbe476e2
Tyler Liskiewicz <tyler.liskiewicz@gmail.com>
2025-04-14 07:10:59 +0000

    Creating AccountDAO class and setting up imports

---
+++ src/main/java/DAO/AccountDAO.java
@@ -0,0 +1,15 @@
+package DAO;
+
+import Model.Account;
+import Util.ConnectionUtil;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * Account DAO class to handle the user login/registration data from the front-end and interact with the database
+ */
+
+public class AccountDAO {
+    
+}
